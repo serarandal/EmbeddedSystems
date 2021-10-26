a30b34deaab6b36eee260613b6542af80b3a9df2
sergio <serarandal@gmail.com>
2021-10-26 18:34:46 +0200

    Add files via upload

--- p4/newmain1.c
+++ p4/newmain1.c
@@ -22,13 +22,13 @@ void init_pwm()
     T2CONbits.TMR2ON=1;  
     CCP1CONbits.P1M=0b00;   //Single output;P1A modulated; P1B,P1C,P1D assigned as port pins
     CCP1CONbits.CCP1M=0b1100; // PWM mode; P1A,P1C active-high; P1B,P1D active-high
-    TRISCbits.TRISC2=1;
+    TRISCbits.TRISC2=0;
     CCPR1L=0;
 }
 void init_ccp2()
 {
     CCP2CONbits.CCP2M = 0b1100;
-    TRISCbits.TRISC1 = 1;
+    TRISCbits.TRISC1 = 0;
     CCPR2L = 0;
 
 }
@@ -38,9 +38,9 @@ void init_T0()
     OPTION_REGbits.T0CS = 0;
     OPTION_REGbits.PSA = 0;
     OPTION_REGbits.PS = 0b111;
-    TMR0=157;
+    TMR0=0;
 }
-void init_T1()
+void init_T2()
 {
     TMR2IF=0;
     T2CONbits.T2CKPS=0;
@@ -52,40 +52,37 @@ void __interrupt() intr_T0(void)
     
     if(TMR0)
     {
-        if(modo == 0)
+        aux++;
+        TMR0=0;
+        if(aux == 632)
         {
-            CCPR1L++;
-            CCPR2L--;
+            aux = 0;
+            TMR0 = 0;
+            if(modo == 0)
+            {
+                T0IF=0;
+                CCPR1L++;
+                CCPR2L=166-CCPR1L;
+                if(CCPR1L==166)
+                {
+                    modo =1;
+                }
+            }
+            else if(modo == 1)
+            {
+                T0IF=0;
+                CCPR2L++;
+                CCPR1L=166-CCPR2L;
+                if(CCPR1L==0)
+                 {
+                    modo = 0;
+                 }
+            }
+            
         }
-        else if(modo ==1)
-        {
-            CCPR1L--;
-            CCPR2L++;
-        }
-        
-     TMR0=0; 
+ 
     }
-    if(TMR2IF)
-    {
-        if(aux==1)
-        {
-            modo=0;
-            TRISCbits.TRISC2=0;
-            TRISCbits.TRISC1=0;
-            TMR2IF=0;
-        }
-        else
-        {
-            aux = 1;
-            modo =1;
-            TRISCbits.TRISC2=0;
-            TRISCbits.TRISC1=0;
-            TMR2IF = 0;  
-        }
-        
-    } 
-    
-
+     
     
 }
 
@@ -96,7 +93,7 @@ void main(void) {
     init_pwm();
     init_ccp2();
     init_T0();
-    init_T1();
+    init_T2();
     while(1);
     
     
